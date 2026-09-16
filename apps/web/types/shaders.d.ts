dadc5e6c88f2f4fd03a68971217431d169b2c590
milapmodi101 <milapmodi1111@gmail.com>
2026-09-16 17:31:43 +0530

    Revert "feat: integrate MailForensix landing page, design system, WebGL Aurora shader, and auth shell into apps/web"
    This reverts commit 1bb43197cda657fa5625e90413a8f0fb92ce0472.

--- apps/web/types/shaders.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "shaders/react" {
-  export const Shader: any;
-  export const SolidColor: any;
-  export const Surface3D: any;
-  export const DotGrid: any;
-  export const Prism: any;
-  export const LinearGradient: any;
-  export const LiquidMetal: any;
-}
